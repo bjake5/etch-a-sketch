3cd5ceb0725ebcf8ba973119635e5aac106d3d70
Blake <21201344+bjake5@users.noreply.github.com>
2024-07-24 12:48:08 -0400

    Add comments for code readability

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+
+// Set constants from HTML page
 const gridContainer = document.querySelector("#grid-container");
 const gridSizeSettings = document.querySelector("#grid-size-settings");
 const colorSettings = document.querySelector("#color-settings");
@@ -6,32 +8,34 @@ const userCurrentColorSelection = document.getElementById("user-current-color");
 const isRandomColorEnabled = document.getElementById("random-color-selection");
 const randomCurrentColorPreview = document.querySelector("#random-color-selection-preview");
 const isEraserEnabled = document.getElementById("eraser-selection");
-
 const isMouseoverEnabled = document.getElementById("mouseover-grid-nav-option");
 const isOnClickEnabled = document.getElementById("onclick-grid-nav-option");
 
+// Create new constants
 const currentGridSizeLabel = document.createElement("div");
 const changeGridSizeButton = document.createElement("button");
 const eraseGridButton = document.createElement("button");
 
-
+//Set temporary variables for grid size and default viewport height
 let gridViewportHeight = 600;
 let rows = 16;
 let columns = 16;
 
+// Immediately create the grid layout and then call the function to enable etch-a-sketch capabilities
 createGridLayout();
 
+// Create the grid
 function createGridLayout() {
-    let height = screen.height;
-    let width = screen.width;
-    if (height > width) gridViewportHeight = width * .6;
-    if (width > height) gridViewportHeight = height * .6;
+    let height = screen.height; // Determine screen height
+    let width = screen.width; // Determine screen width
+    if (height > width) gridViewportHeight = width * .6; // Ensure the grid fits on the screen
+    if (width > height) gridViewportHeight = height * .6; // Ensure the grid fits on the screen regardless of screen height or screen width
     gridContainer.style.height = gridViewportHeight + "px";
     gridContainer.style.width = gridViewportHeight + "px";
-    let squareHeight = gridViewportHeight / rows + "px";
-    let numberOfSquares = rows * columns; 
+    let squareHeight = gridViewportHeight / rows + "px"; // Determine how big the grid can be
+    let numberOfSquares = rows * columns; // Determine number of squares to populate the grid
     
-    // Setup grid settings
+    // Setup grid settings and basic copy
      currentGridSizeLabel.textContent = `${rows} x ${columns} | ${numberOfSquares} squares`;
      eraseGridButton.textContent = `Erase Grid`;
      changeGridSizeButton.textContent = `Change Grid Size`;
@@ -93,24 +97,25 @@ function runEtchASketch () {
     });
 };
 
+// Function used to change the grid upon button click
 function changeGridLayout() {
-    let newGridSize = Number(prompt("How many squares would you like on each side?"));
-    while (newGridSize > 100) {
+    let newGridSize = Number(prompt("How many squares would you like on each side?")); // Ask user for new grid size
+    while (newGridSize > 100) { // Reject grid sizes over 100 squares
         newGridSize = Number(prompt("How many squares would you like on each side?"));
     }
     rows = newGridSize;
     columns = newGridSize;
-    gridContainer.replaceChildren();
-    gridSizeSettings.replaceChildren();
+    gridContainer.replaceChildren(); // Remove all of the existing squares
+    gridSizeSettings.replaceChildren(); 
     createGridLayout();
 };
 
 function eraseGrid (){
-    gridContainer.replaceChildren();
+    gridContainer.replaceChildren(); // Remove all of the existing squares
     createGridLayout();
 }
 
-function changeColor () {
+function changeColor () { // Function to determine whether to use user-selected color, random color, or eraser functionality
     if(isUserColorEnabled.checked == true) {
         event.target.style.backgroundColor = userCurrentColorSelection.value;
     } else if(isRandomColorEnabled.checked == true) {
